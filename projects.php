c648ec54e12b05b97978ec996e9806e013a04202
Dwayne Linde <lindesara@gmail.com>
2016-04-30 14:10:02 -0700

    updated projects.php

--- projects.php
+++ projects.php
@@ -6,44 +6,6 @@
     
     $cart = new Cart();
     setlocale(LC_MONETARY, 'en_US');
-
-/* 
-$toAddress = "spinifex@gmail.com";  
-$toName = "ITC 240"; 
-$website = "Spinifex Questionnaire";  
-#--------------END CONFIG AREA ------------------------#
-$sendEmail = TRUE; 
-$dateFeedback = true; 
-include_once 'includes/config.php';
-include 'includes/contact-lib/contact_include.php';
-$response = null;
-$reCaptcha = new ReCaptcha($secretKey);
-if (isset($_POST["g-recaptcha-response"]))
-{// if submitted check response
-    $response = $reCaptcha->verifyResponse(
-        $_SERVER["REMOTE_ADDR"],
-        $_POST["g-recaptcha-response"]
-    );
-}
-if ($response != null && $response->success)
-    {#reCAPTCHA agrees data is valid (PROCESS FORM & SEND EMAIL)
-        handle_POST($skipFields,$sendEmail,$toName,$fromAddress,$toAddress,$website,$fromDomain);             
-    ?>
-    <!-- START HTML FEEDBACK -->
-    <div class="contact-feedback">
-        <h2>We have recieved your response!</h2>
-        <p>Thanks for the input!</p>
-        <p>We'll respond within 48 hours, if you requested information.</p>
-    </div>    
-    <!-- END HTML FEEDBACK -->        
-    <?php
-}else{#show form, either for first time, or if data not valid per reCAPTCHA 
-    if($response != null && !$response->success)
-    {
-        $feedback = dateFeedback($dateFeedback);
-        send_POSTtoJS($skipFields); #function for sending POST data to JS array to reload form elements
-    }//end failure feedback
-*/  
 ?>
 
 <h1>Order Your Baja Tacos Here!</h1><p>
@@ -110,29 +72,7 @@ if ($response != null && $response->success)
         ?>
         
     <p></p>    
-<!--
-    <div>
-		<label>
-			Name:<br /><input type="text" name="Name" required="required" placeholder="Full Name (required)" title="Name is required" tabindex="10" size="44" autofocus />
-		</label>
-	</div>
-	<div>	
-		<label>
-			Phone:<br /><input type="tel" name="Phone" required="required" placeholder="Phone (required)" title="A valid phone is required" tabindex="20" size="44" />
-		</label>
-	</div>
-    	<div>	
-		<label>
-			Email:<br /><input type="email" name="Email" required="" placeholder="Email" title="A valid email is requested" tabindex="20" size="44" />
-		</label>
-	</div>    
-    <p></p>    
-	<div>	
-		<label>
-			Comments:<br /><textarea name="Comments" cols="36" rows="4" placeholder="Your comments are important to us, thank you!" tabindex="60"></textarea>
-		</label>
-	</div>	
--->
+
     <div class="g-recaptcha" data-sitekey="<?=$siteKey;?>"></div>
 	<div>
 		<input type="Submit" value="Order your delicious food here!" />
@@ -144,8 +84,3 @@ if ($response != null && $response->success)
     </script>
 </section>
 <?php include 'includes/aside2.php';?>
-<?php 
-/*
-include 'includes/footer.php';
-*/
-?>
